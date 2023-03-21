63aa81b521aaf6a871786b41e70984ac1ce3bf9e
Julio Donato <54926442+jdonato94@users.noreply.github.com>
2023-03-21 12:40:12 -0300

    Add files via upload

---
+++ minutoparahora.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def main(): 
+    print("Conversor Minuto para Hora")
+    conversor = conversor_hora()
+    
+   
+    
+def conversor_hora():
+    tempo = int(input("Digite a quantidade de minutos que voce quer converter para horas "))
+    tempo = tempo
+    hora = (tempo / 60)
+    if hora < 1:
+        hora = 0
+    else:
+        hora = round(hora)    
+    minuto = tempo % 60
+    print(f"{tempo} minutos equivale a {hora} horas e {minuto} minutos.")
+
+
+
+if __name__ == "__main__":
+    main()
+        
+
+
+# In[ ]:
+
+
+
+
